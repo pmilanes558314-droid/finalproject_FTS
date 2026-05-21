63f389d2da5e5e78d0d587bb0472ad58795a29fb
pmilanes558314 <p.milanes.558314@umindanao.edu.>
2026-05-21 18:57:05 +0800

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,20 +4,20 @@ use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
 class AppServiceProvider extends ServiceProvider
 {
-/**
-* Register any application services.
-*/
-public function register(): void
-{
-//
-}
-/**
-* Bootstrap any application services.
-*/
-public function boot(): void
-{
-if (env('APP_ENV') === 'production') {
-URL::forceScheme('https');
-}
-}
+    /**
+    * Register any application services.
+    */
+    public function register(): void
+    {
+    //
+    }
+    /**
+    * Bootstrap any application services.
+    */
+    public function boot(): void
+    {
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
+    }
 }
